44824d753097b6888a40934c4ea778d90da3b6d7
Bruno Osvaldo Hernandez Esquivel <brunoh77@outlook.com>
2015-11-03 19:57:30 -0800

    got classloader to wkr

--- src/DesignAnalyzer.java
+++ src/DesignAnalyzer.java
@@ -4,38 +4,79 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Set;
 
 /**
  * WHo cares
  * Created by bruno on 10/30/15.
  */
-public class DesignAnalyzer {
+public class DesignAnalyzer
+{
   //Set<> pack = null;
-  public DesignAnalyzer() {
+  public DesignAnalyzer()
+  {
 
   }
 
-  void loadPackage( String path ) {
-    System.out.println( "blah" );
-    System.out.println( "blah more baby" );
-    System.out.println( "Gathering class files in " + path );
-    FilenameFilter classFilter = new FilenameFilter() {
-      public boolean accept( File dir, String name ) {
-        return name.toLowerCase().endsWith( ".class" );
+  void loadPackage(String path)
+  {
+    System.out.println("Gathering class files in " + path);
+    FilenameFilter classFilter = new FilenameFilter()
+    {
+      public boolean accept(File dir, String name)
+      {
+        return name.toLowerCase().endsWith(".class");
       }
     };
-    File f = new File( path ); // the directory, really!
-    for ( File file : f.listFiles( classFilter ) )
-      System.out.println( file.getName() );
+
+    File f = new File(path); // the directory, really!
+    for (File file : f.listFiles(classFilter))
+    {
+      System.out.println(file.getName());
+      //Class cl = file.
+    }
   }
 
-  public static void main( String[] args ) throws IOException {
+  public void secondLoader(String path)
+  {
+    File file = new File(path);
+    System.out.println("BLAHBLAH BLAH");
+    try
+    {
+      // Convert File to a URL
+      URL url = file.toURI().toURL();          // file:/c:/myclasses/
+      URL[] urls = new URL[]{url};
+
+      // Create a new class loader with the directory
+      ClassLoader cl = new URLClassLoader(urls);
+
+      // Load in the class; MyClass.class should be located in
+      // the directory file:/c:/myclasses/com/mycompany
+      Class cls = cl.loadClass("demo.C");
+      System.out.println("lalalal:" +  cls.getSuperclass().getName());
+    } catch (MalformedURLException e)
+    {
+      e.printStackTrace();
+
+    } catch (ClassNotFoundException e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException
+  {
     DesignAnalyzer fl = new DesignAnalyzer();
-    if ( args.length != 1 ) {
-      System.out.println( "Usage FileLoader <path>" );
-    } else {
-      fl.loadPackage( args[ 0 ] );
+    if (args.length != 1)
+    {
+      System.out.println("Usage FileLoader <path>");
+    } else
+    {
+      fl.loadPackage(args[0]);
+      fl.secondLoader(args[0]);
     }
   }
 }
