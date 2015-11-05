c70d4b45a4987c52c5b3a8de9a3af7ea2a9d7538
bruno <brunoh77@outlook.com>
2015-11-05 14:52:33 -0800

    Working version of project

--- src/DesignAnalyzer.java
+++ src/DesignAnalyzer.java
@@ -1,76 +1,61 @@
-import com.sun.java.util.jar.pack.*;
-
-import java.awt.*;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * WHo cares * Created by bruno on 10/30/15.
  */
-public class DesignAnalyzer
-{
+public class DesignAnalyzer {
 
-  private List<Class> classNames = null;
+  private List<Class> classes = null;
   private double packageMethodCount = 0;
-  public DesignAnalyzer()
-  {
 
-    classNames = new ArrayList<>();
+  public DesignAnalyzer() {
+    classes = new ArrayList<>();
   }
 
-  List<String> getClassNames(String path)
-  {
-    FilenameFilter classFilter = new FilenameFilter()
-    {
-      public boolean accept(File dir, String name)
-      {
+  List<String> getClassNames(String path) {
+    FilenameFilter classFilter = new FilenameFilter() {
+      public boolean accept(File dir, String name) {
         return name.toLowerCase().endsWith(".class");
       }
     };
 
     List<String> names = new ArrayList<>();
 
-    for (File file : (new File(path)).listFiles(classFilter))
-    {
-      if(file != null){
+    for (File file : (new File(path)).listFiles(classFilter)) {
+      if (file != null) {
         names.add(file.getName().replaceAll(".class", ""));
       }
     }
     return names;
   }
 
-
-
-  public void loadPackage(String path)
-  {
+  public void loadPackage(String path) {
     String[] split = path.split("/");
-    for(String s: split){
-      System.out.println(s);
+    for (String s : split) {
     }
     String dir = split[0];
     List<String> names = getClassNames(path);
-    if(names == null){
+    if (names == null) {
       return;
     }
     String packageName = split[1];
-    File file = new File(dir);
-    System.out.println(dir + " " + packageName);
-    System.out.println(names.size());
-    try
-    {
-      // Convert File to a URL
 
+    File file = new File(dir);
+
+    try {
+      // Convert File to a URL
       URL url = file.toURI().toURL();          // file:/c:/myclasses/
-      System.out.println("after url " + file);
       URL[] urls = new URL[]{url};
 
       // Create a new class loader with the directory
@@ -79,65 +64,122 @@ public class DesignAnalyzer
       // Load in the class; MyClass.class should be located in
       // the directory file:/c:/myclasses/com/mycompany
 
-      for(String name: names){
+      for (String name : names) {
         //System.out.println(packageName +"."+ name.replaceAll(".class","") + "->" + cl);
-        classNames.add(cl.loadClass(packageName +"."+ name));
-        packageMethodCount += classNames.get(classNames.size()-1).getDeclaredMethods().length;
+        classes.add(cl.loadClass(packageName + "." + name));
+        packageMethodCount += classes.get(classes.size() - 1).getDeclaredMethods().length;
       }
+
       //Class cls = cl.loadClass("demo"+"."+"C");
-      System.out.println("class size: " + classNames.size());
-      System.out.println("method count: " + packageMethodCount);
-    } catch (MalformedURLException e)
-    {
+    } catch (MalformedURLException e) {
       e.printStackTrace();
 
-    } catch (ClassNotFoundException e)
-    {
+    } catch (ClassNotFoundException e) {
       e.printStackTrace();
-    } catch (NullPointerException e){
+    } catch (NullPointerException e) {
       System.out.println("^ null pointer");
     }
   }
 
-  private void printClasses(){
-    for(int i=0;i<classNames.size();i++){
-      System.out.println(i + " " + classNames.get(i).getName());
-    }
-  }
-
-  public static void main(String[] args) throws IOException
-  {
+  public static void main(String[] args) throws IOException {
     DesignAnalyzer fl = new DesignAnalyzer();
-    if (args.length != 1)
-    {
+    if (args.length != 1) {
       System.out.println("Usage FileLoader <path>");
-    } else
-    {
+    } else {
       fl.loadPackage(args[0]);
-      fl.classWorkLoad();
-      //fl.secondLoader(args[0]);
+      fl.displayMetrics();
     }
   }
 
-  public int classWorkLoad(){
-    int i = 0;
-    int j = 0;
-    int count = 0;
-    for(Class cl : classNames){
-      //System.out.println( (++j) + " name: " + getMethodCount(cl));
-      System.out.printf("%3d%-10s%.2f%n",(++j),"name",getMethodCount(cl));
-      count += getMethodCount(cl);
+  /**
+   * helper method to responsibility
+   *
+   * @param a class who is doing referencing
+   * @param b class that is being potentially referenced
+   * @return
+   */
+  private boolean isClient(Class a, Class b) {
+    Method[] methods = a.getDeclaredMethods();
+    Field[] fields = a.getDeclaredFields();
+    for (Field field : fields) {
+      if (field.getType().getName().equals(b.getName())) {
+        return true;
+      }
     }
-    System.out.println("count: " + count);
-    return -1;
+    for (Method method : methods) {
+      if (method.getReturnType().getName().equals(b.getName())) {
+        return true;
+      } else if (method.getName().equals("main") && method.getReturnType().getName().equals("void")
+          && Modifier.isStatic(method.getModifiers()) && Modifier.isPublic(method.getModifiers())) {
+        // TODO: 11/5/15 Handle mains instantiations here if possible
+      }
+      for (Class c : method.getParameterTypes()) {
+        if (c.getName().equals(b.getName())) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public void displayMetrics() {
+    System.out.printf("%-5s%-20s%-20s%-20s%-20s%n", "C", "inDepth(C)", "instability(C)", "responsibility(C)", "workload(C)");
+    for (Class C : classes) {
+      System.out.printf("%-5s%-20d%-20.2f%-20.2f%-20.2f%n", C.getName().substring(C.getName().length() - 1), inDepth(C), instability(C), responsiblity(C), workload(C));
+    }
+  }
+
+  /**
+   * counts clients of the class cl
+   *
+   * @param cl
+   * @return
+   */
+  private double responsiblity(Class cl) {
+    int count = 0;
+    for (Class aClass : classes) {
+      if (!aClass.getName().equals(cl.getName())) {
+        if (isClient(aClass, cl)) {
+          count++;
+        }
+      }
+    }
+    return ((double) ((int) (100 * (count / ((double) classes.size()))))) / 100;
+  }
+
+  private double instability(Class cl) {
+    int count = 0;
+    if(!cl.getSuperclass().getName().equals("java.lang.Object")){
+      count++;
+    }
+    for (Class aClass : classes) {
+      if (!aClass.getName().equals(cl.getName())) {
+        if (isClient(cl,aClass)) {
+          count++;
+        }
+      }
+    }
+    return ((double) ((int) (100 * (count / ((double) classes.size()))))) / 100;
   }
 
   /**
    * used the (int) cast to truncate value.
+   * gets workload
+   *
    * @param cl
    * @return
    */
-  private double getMethodCount(Class cl){
-    return ( (double)( (int)( 100*( cl.getDeclaredMethods().length/packageMethodCount ) ) ) )/100;
+  private double workload(Class cl) {
+    return ((double) ((int) (100 * (cl.getDeclaredMethods().length / packageMethodCount)))) / 100;
+  }
+
+  public int inDepth(Class cl) {
+    int count = 1;
+    Class parent = cl.getSuperclass();
+    while (!parent.getName().equals("java.lang.Object")) {
+      count++;
+      parent = parent.getSuperclass();
+    }
+    return count;
   }
 }
